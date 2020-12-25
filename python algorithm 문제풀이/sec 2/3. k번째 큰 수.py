14c013186dd23231c0c4b21bf5ee5ca017212da9
Youngbin Woo <53562677+youngbinwoo@users.noreply.github.com>
2020-12-25 22:12:49 +0900

    Update 3. k번째 큰 수.py

--- python algorithm 문제풀이/sec 2/3. k번째 큰 수.py
+++ python algorithm 문제풀이/sec 2/3. k번째 큰 수.py
@@ -1,9 +1,8 @@
 import sys
-#sys.stdin=open("input.txt", "rt")
+sys.stdin=open("input.txt", "rt")
 ans=[]
 
 n,k=map(int,input().split())
-
 num=list(map(int,input().split()))
 
 for i in range(n-2):
@@ -11,6 +10,8 @@ for i in range(n-2):
         for p in range(j+1,n):
             a=num[i]+num[j]+num[p] 
             ans.append(a)
+
 r_ans=list(set(ans))
 r_ans.sort(reverse=True)
 print(r_ans[k-1])
+
