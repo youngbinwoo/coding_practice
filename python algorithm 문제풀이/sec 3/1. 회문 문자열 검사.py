5791a5d242ead3e808db1b93e10faadfe00d9a6c
Youngbin Woo <53562677+youngbinwoo@users.noreply.github.com>
2020-12-28 18:19:56 +0900

    Update 1. 회문 문자열 검사.py

--- python algorithm 문제풀이/sec 3/1. 회문 문자열 검사.py
+++ python algorithm 문제풀이/sec 3/1. 회문 문자열 검사.py
@@ -13,8 +13,22 @@ for i in range(1,n+1):
         ans='NO'
     print('#%d %s' %(i,ans))
         
+_______________________________________________
+import sys
+sys.stdin=open("input.txt", "rt")
 
+n=int(input())
 
+for i in range(0,n):
+    s=input()
+    s=s.upper()
+    size=len(s)
+    for j in range(size//2):
+        if s[j]!=s[-1-j]:
+            print("#%d NO" %(i+1))
+            break
+        else:
+            print("#%d YES" %(i+1))
 
 
 
