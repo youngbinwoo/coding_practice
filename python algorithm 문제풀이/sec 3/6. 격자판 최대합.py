9c9044ac10eb155c41cce4dd9411502468062790
Youngbin Woo <53562677+youngbinwoo@users.noreply.github.com>
2021-01-07 23:23:10 +0900

    Update 6. 격자판 최대합.py

--- python algorithm 문제풀이/sec 3/6. 격자판 최대합.py
+++ python algorithm 문제풀이/sec 3/6. 격자판 최대합.py
@@ -21,3 +21,32 @@ for i in range(n):
 r.append(c)
 r.append(d)
 print(max(r))
+
+__________________________________________________________________________________________________
+
+import sys
+sys.stdin=open("input.txt", "rt")
+
+n=int(input())
+a=[list(map(int,input().split())) for _ in range(n)]
+largest=-21470000
+for i in range(n):
+    sum1=sum2=0
+    for j in ragne(n):
+        sum1+=a[i][j]
+        sum2+=a[j][i]
+    if sum1>largest:
+        largest=sum1
+    if sum2>largest:
+        largest=sum2
+
+sum1=sum2=0
+for i in range(n):
+    sum1+=a[i][i]
+    sum2+=a[i][n-i-1]
+if sum1>largest:
+    largest=sum1
+if sum2>largest:
+    largest=sum2
+
+    
