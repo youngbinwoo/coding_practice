b54730e72b19730a55fbb55f7d6fc729be69c3b3
Youngbin Woo <53562677+youngbinwoo@users.noreply.github.com>
2021-01-12 23:42:30 +0900

    Update 11.격자판 회문수.py

--- python algorithm 문제풀이/sec 3/11.격자판 회문수.py
+++ python algorithm 문제풀이/sec 3/11.격자판 회문수.py
@@ -15,3 +15,22 @@ for j in range(len(a[0])):
 print(cnt)
 
 
+___________________________________________________________________
+
+import sys
+sys.stdin=open("input.txt", "rt")
+a=[list(map(int,input().split())) for _ in range(7)]
+
+cnt=0
+for i in range(3):
+    for j in range(7):
+        tmp=a[j][i:i+5]
+        if tmp==tmp[::-1]:
+            cnt+=1
+        for k in range(2):
+            if a[i+k][j]!=a[i-k+4][j]:
+                break
+        else:
+            cnt+=1
+
+print(cnt)
