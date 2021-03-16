55a9e3fad572544113ff92384900e87d73f45d4d
Youngbin Woo <53562677+youngbinwoo@users.noreply.github.com>
2021-03-16 20:25:24 +0900

    Create ﻿Anagram(딕셔너리).py

---
+++ python algorithm 문제풀이/sec 5/﻿Anagram(딕셔너리).py
@@ -0,0 +1,16 @@
+import sys
+#sys.stdin=open("in1.txt", "r")
+a=input()
+b=input()
+sH=dict()
+for x in a:
+    sH[x]=sH.get(x, 0)+1
+for x in b:
+    sH[x]=sH.get(x, 0)-1
+
+for x in a:
+    if(sH.get(x)>0):
+        print("NO")
+        break;
+else:
+    print("YES")
