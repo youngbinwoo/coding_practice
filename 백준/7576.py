56b1222f15a1f91cfd5b7c897f6297edc3e6e547
Youngbin Woo <53562677+youngbinwoo@users.noreply.github.com>
2021-07-16 15:58:20 +0900

    Update 7576.py

--- 백준/7576.py
+++ 백준/7576.py
@@ -1,48 +1,39 @@
 import sys
-sys.setrecursionlimit(10**6)
 from collections import deque
 
-def bfs(x,y):
-    q=deque()
-    q.append((x,y))
-
-    while q:
-        a,b=q.popleft()
-        for i in range(4):
-            xx=dx[i]+a
-            yy=dy[i]+b
-            if 0<=xx<m and 0<=yy<n:
-                if num[xx][yy]==0 and r_ch[xx][yy]==0:
-                    r_ch[xx][yy]=1
-                    ch[xx][yy]=min(ch[xx][yy],ch[a][b]+1)
-                    q.append((xx,yy))
+n,m=map(int,input().split())
+num=list(list(map(int,input().split())) for _ in range(m))
+ch=[[0]*n for _ in range(m)]
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+q=deque()
 
 
-if __name__=="__main__":
-    n,m=map(int,input().split())
-    num=[]
-    r=10000
-    ch=[[r]*n for _ in range(m)]
-    dx=[-1,0,1,0]
-    dy=[0,-1,0,1]
-    for _ in range(m):
-        num.append(list(map(int,input().split())))
+for i in range(m):
+    for j in range(n):
+        if num[i][j]==1 and ch[i][j]==0:
+            q.append((i,j))
+while q:
+    a,b=q.popleft()
+    for i in range(4):
+        xx=a+dx[i]
+        yy=b+dy[i]
+        if 0<=xx<m and 0<=yy<n:
+            if num[xx][yy]==0 and ch[xx][yy]==0:
+                num[xx][yy]=1
+                ch[xx][yy]=ch[a][b]+1
+                q.append((xx,yy))
 
-    for i in range(m):
-        for j in range(n):
-            if num[i][j]==1:
-                ch[i][j]=0
-                r_ch=[[0]*n for _ in range(m)]
-                r_ch[i][j]=1
-                bfs(i,j)
-            if num[i][j]==-1:
-                ch[i][j]=-1
-    
-    Max=0
-    for z in range(m):
-        Max=max(Max,max(ch[z]))
-    
-    if Max==r:
-        print(-1)
-    else:
-        print(Max)
+f=0
+Max=0
+for i in range(m):
+    for j in range(n):
+        if num[i][j]==0:
+            f=1
+            break
+    Max=max(Max,max(ch[i]))
+
+if f==1:
+    print(-1)
+else:
+    print(Max)
