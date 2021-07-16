ccdcc97a04bde753deea1c1082ffc87538c27c51
Youngbin Woo <53562677+youngbinwoo@users.noreply.github.com>
2021-07-16 16:10:37 +0900

    Create 15657.py

---
+++ 백준/15657.py
@@ -0,0 +1,22 @@
+def DFS(L):
+    global aa
+    if L==b:
+        for i in range(len(res)):
+            print(res[i],end=' ')
+        print()
+
+    else:
+        for i in range(len(num)):
+            if aa<=num[i]:
+                aa=num[i]
+                res[L]=num[i]
+                DFS(L+1)
+                aa=0
+
+if __name__=="__main__":
+    aa=0
+    a,b=map(int,input().split())
+    num=list(map(int,input().split()))
+    num.sort()
+    res=[0]*b
+    DFS(0)
