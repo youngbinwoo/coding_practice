779d87ea4ae4c0de96a85a211a44df1da9e5458e
Youngbin Woo <53562677+youngbinwoo@users.noreply.github.com>
2021-07-16 16:00:41 +0900

    Create 11722.py

---
+++ 백준/11722.py
@@ -0,0 +1,13 @@
+n=int(input())
+num=list(map(int,input().split()))
+dp=[0]*n
+dp[0]=1
+
+for i in range(1,n):
+    for j in range(i):
+        if num[i]<num[j]:
+            dp[i]=max(dp[i],1+dp[j])
+    if dp[i]==0:
+        dp[i]=1
+
+print(max(dp))
