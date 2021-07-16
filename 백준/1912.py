05ca525946127e690f443fc8695d11e7be631c73
Youngbin Woo <53562677+youngbinwoo@users.noreply.github.com>
2021-07-16 16:03:07 +0900

    Update 1912.py

--- 백준/1912.py
+++ 백준/1912.py
@@ -1,19 +1,12 @@
 n=int(input())
 num=list(map(int,input().split()))
-num.insert(0,0)
-dp=[[0]*(n+1) for _ in range(n+1)]
-m=-1001
+dp=[0]*n
+dp[0]=num[0]
 
-for i in range(n+1):
-    for j in range(i+1,n+1):
-        if i==0:
-            dp[i][j]=num[j]
-            if dp[i][j]>m:
-                m=dp[i][j]
+for i in range(1,len(num)):
+    if num[i]>dp[i-1]+num[i]:
+        dp[i]=num[i]
+    else:
+        dp[i]=dp[i-1]+num[i]
 
-        else:
-            dp[i][j]=num[j]+dp[i-1][j-1]
-            if dp[i][j]>m:
-                m=dp[i][j]
-
-print(m)
+print(max(dp))
